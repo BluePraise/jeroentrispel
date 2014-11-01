5a5c61e4a9d32a36a7068ad97420e41235116acd
BluePraise <magaliechetrit@gmail.com>
2014-11-01 23:59:32 +0100

    frontpage small fixes

--- front-page.php
+++ front-page.php
@@ -3,17 +3,12 @@
 * Template Name: Front Page
 */
 get_header();
-?>
-
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+if ( have_posts() ) : while ( have_posts() ) : the_post();
       // the_content();
       endwhile;
       wp_reset_query();
       endif;
-    ?>
 
-<?php
-// else:
 
 get_footer();
 ?>
