2d677cf72c1eb232c03d25f724fe60982e5a6ae9
Magalie Chetrit <magaliechetrit@gmail.com>
2014-12-01 21:45:39 +0100

    Single Blog Page

--- single.php
+++ single.php
@@ -12,15 +12,24 @@ get_header(); ?>
             <div class="entry-header">
 
                 <h2 class="entry-title"><?php the_title(); ?></h2>
-                      <?php if ( ! post_password_required()) :
-                      edit_post_link( __( 'Edit', '' ), '<span class="edit-link">', '</span>' );
-                      endif;
-                ?>
+                <div class="entry-meta">
+                  <span class="entry-date">Geplaatst op: <?php the_date();?></span>
+                  <span class="entry-author"><?php the_author();?></span>
+                </div>
 
+                  <?php if ( ! post_password_required()) :
+                          jeroentrispel_edit_link();
+                  endif;?>
+            
             </div><!-- .entry-header -->
+                
+            <?php if ( has_post_thumbnail( $post_id )) : // If an article has a featured image
+                echo "<span class='page-feature-image'>";
+                  the_post_thumbnail( );
+                echo "</span>";
+              endif; ?>
 
             <div class="entry-content">
-
               <?php the_content(); ?>
             </div><!-- .entry-content -->
 
@@ -29,8 +38,8 @@ get_header(); ?>
               <div class="page-nav">
                 <!-- IF MORE THAN ONE POST THAN SHOW LINK -->
                 <ul>
-                  <li class="prev-link icomayconnect"><?php previous_post_link(' %link'); ?></li>
-                  <li class="next-link icomayconnect"><?php next_post_link(' %link'); ?></li>
+                  <li class="prev-link"><?php previous_post_link(' %link'); ?></li>
+                  <li class="next-link"><?php next_post_link(' %link'); ?></li>
                 </ul>
               </div>
 
